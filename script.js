955f87a809013d247e3672622eff7c30a510fe6a
khalidjambi <khalid.jambi18@gmail.com>
2019-02-17 09:54:48 +0700

    hallo

---
+++ script.js
@@ -0,0 +1,78 @@
+/********************
+ * variables and data types
+ */
+/*
+var firstname = 'Khalid';
+console.log('firstName');
+
+var lastname = 'Boy';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+
+// variabels naming rules
+var _3years = 3;
+var johnMark = 'John and Mark';
+var if = 23;
+*/
+
+ /**************************** 
+  * variable mutation and type coercion
+*/
+/*
+var firstName = 'Khalid';
+var age = 19;
+
+// Type coercion
+console.log (firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a  ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// variable mutation
+age = 'twenty';
+job = 'driver';
+alert(firstName + ' is a  ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('what is his last name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+/******************************
+ * Basic operators
+ */
+
+ //Math operators
+var year, yearJohn, yearMark;
+ now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+ var yearJohn = now - ageJohn;
+ var yearMark = now - ageMark;
+
+ console.log(yearJohn);
+
+ console.log(now + 2);
+ console.log(now * 2);
+ console.log(now / 10);
+
+//  Logical operators 
+
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+
+//type of operator 
+console.log(typeof johnOlder);
